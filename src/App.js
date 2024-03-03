97acb02c88356100a3967238caccad675f1d4b9d
rajamanickam.e <rajamanickam.e@xerago.com>
2024-03-03 18:51:56 +0530

    Readme file update

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import Tabs from './Components/Tabs';
 
 function App() {
+
+  const tabs = [
+    {
+      title: 'Accounts',
+      content: 'Money owed by a business to its suppliers shown as a liability on a companys balance sheet. It is distinct from notes payable liabilities, which are debts created by formal legal instrument documents.'
+    },
+    {
+      title: 'Payroll',
+      content: 'Payroll is the total of all compensation a business must pay to its employees for a set period of time or on a given date.'
+    },
+    {
+      title: 'Tax',
+      content: 'A tax refund or tax rebate is a payment to the taxpayer due to the taxpayer having paid more tax than they owed.'
+    },
+  ];
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Tabs tabs={tabs} />
+    </>
   );
 }
 
